6595c264bb127613990008cb0aa0804b7f3e5180
Sergey Opivalov <sopivalov@gradle.com>
2022-11-29 20:51:56 +0300

    Add JUnit multiversion test

---
+++ subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/JUnitDryRunIntegrationTest.groovy
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.testing.junit
+
+import org.gradle.integtests.fixtures.DefaultTestExecutionResult
+import org.gradle.integtests.fixtures.TargetCoverage
+import org.gradle.integtests.fixtures.TestExecutionResult
+import org.gradle.testing.fixture.JUnitMultiVersionIntegrationSpec
+
+import static org.gradle.testing.fixture.JUnitCoverage.getJUNIT_4_LATEST
+import static org.gradle.testing.fixture.JUnitCoverage.getJUPITER
+import static org.gradle.testing.fixture.JUnitCoverage.getVINTAGE
+
+@TargetCoverage({ JUNIT_4_LATEST + [JUPITER, VINTAGE] })
+class JUnitDryRunIntegrationTest extends JUnitMultiVersionIntegrationSpec {
+
+    def "dry run test is skipping execution and considering as passed in report"() {
+        given:
+        buildFile << """
+        apply plugin: 'java-library'
+        ${mavenCentralRepository()}
+
+        dependencies { ${dependencyNotation.collect { "testImplementation '$it'" }.join('\n')} }
+        """
+
+        and:
+        file("src/test/java/SomeTest.java") << """
+        import org.junit.*;
+
+        public class SomeTest {
+            @Test public void failingTest() {
+                throw new RuntimeException();
+            }
+        }
+        """
+        TestExecutionResult executionResult = new DefaultTestExecutionResult(testDirectory)
+
+        expect:
+        succeeds("test", "--test-dry-run")
+        executionResult.testClass("SomeTest").assertTestPassed("failingTest")
+    }
+}
