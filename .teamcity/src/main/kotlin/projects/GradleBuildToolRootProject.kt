168849308f3464382c067edead9e551bf89c7e1a
Bo Zhang <bo@gradle.com>
2022-12-14 10:03:17 +0800

    --no-configuration-cache

--- .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
+++ .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
@@ -18,6 +18,7 @@ class GradleBuildToolRootProject(branch: VersionedSettingsBranch) : Project({
             gradleWrapper(this@BuildType) {
                 name = "GRADLE_RUNNER"
                 tasks = "clean cli:test"
+                gradleParams = "--no-configuration-cache"
             }
         }
     }))
